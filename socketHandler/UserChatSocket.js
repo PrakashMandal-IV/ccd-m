2e8458c4f278c17598e41f0294bdf80980afcc2f
Prakash Mandal <prakashmandal.iv@gmail.com>
2024-03-15 10:49:07 +0530

    Add : controller page change

--- socketHandler/UserChatSocket.js
+++ socketHandler/UserChatSocket.js
@@ -1,5 +1,5 @@
 
-const { addMessageInConversation } = require('../controllers/conversationController');
+const { addMessageInConversation, getDirectChatInbox } = require('../controllers/directChatController');
 const serverStore = require('../serverStore')
 const chatSocket = (socket, io) => {
     try {
@@ -13,6 +13,10 @@ const chatSocket = (socket, io) => {
               emitMessageToUser(response.participents, response.data, io)  // send message to the sender and the reciever
         });
 
+        socket.on("direct-inbox", async () => {  // recieve a message from user
+            var response = await getDirectChatInbox(userId)
+        });
+
     } catch (error) {
         console.log("Error occurred in socket", error.message);
     }
@@ -22,6 +26,13 @@ function emitMessageToUser(Users, message, io) {
     const connectionId = serverStore.getActiveConnections(Users.map(x => x.toString()));
     connectionId.forEach(x => {
         io.to(x).emit("receive-direct-message", message); // broadcast message to the selected users if they are active
+    })
+}
+
+function emitDirectInboxToUser(Users, inbox, io) {
+    const connectionId = serverStore.getActiveConnections(Users.map(x => x.toString()));
+    connectionId.forEach(x => {
+        io.to(x).emit("receive-direct-inbox", inbox); // broadcast message to the selected users if they are active
     })
 }
 module.exports = {
