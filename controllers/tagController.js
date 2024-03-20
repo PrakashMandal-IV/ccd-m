f8b0ec40346ef4a745a6183f25dbd78c426df059
Prakash Mandal <prakashmandal.iv@gmail.com>
2024-03-20 19:37:52 +0530

    Add : fetch my messages

--- controllers/tagController.js
+++ controllers/tagController.js
@@ -2,7 +2,7 @@
 const MessagesModal = require("../models/MessagesModal");
 const TagsModel = require("../models/TagsModel");
 const UserModel = require("../models/UserModel");
-const { GetObjectID } = require("../utils/utilities");
+const { GetObjectID, BuildMessegeObject } = require("../utils/utilities");
 
 
 
@@ -32,7 +32,7 @@ exports.getMembersFortheTags = async (userId, tag, data) => {
 
 
 
-           // add message for the channel 
+            // add message for the channel 
             const messagePayload = new MessagesModal({
                 message: data.message,
                 author: GetObjectID(userId),
@@ -40,7 +40,7 @@ exports.getMembersFortheTags = async (userId, tag, data) => {
                 attachments: data?.attachments || [],
                 type: data.type,
                 messageTypeName: 'TAG_TYPE',
-                messageTypeID: tag._id
+                messageTypeID: Tag._id
             })
             await messagePayload.save()
 
@@ -53,3 +53,21 @@ exports.getMembersFortheTags = async (userId, tag, data) => {
 }
 
 
+exports.getTagMessages = async (req, res) => {
+    try {
+        const userId = req.userId;
+        var tag = req.params.tag;
+        if (!tag?.startsWith("#")) {
+            tag = '#' + tag
+        }
+        const Tag = await TagsModel.findOne({ tagName: tag })
+        const Messages = await MessagesModal.find({ messageTypeName: "TAG_TYPE", messageTypeID: Tag._id,author:GetObjectID(userId)}).sort({ sendTime: 'ascending' }).populate('author', 'name refId').exec()
+        var messages = []
+        Messages.forEach(x => {
+            messages.push(BuildMessegeObject(x))
+        })
+        return res.success("Success", messages);
+    } catch (error) {
+        return res.error("Error occurred while removing member", error.message);
+    }
+}
--- routes/tags.routes.js
+++ routes/tags.routes.js
@@ -6,5 +6,5 @@ const auth = require("../middleware/auth");
 const tagsConroller = require('../controllers/tagController')
 
 router.get("/getMyTags",auth, tagsConroller.getMyTags)
-
+router.get("/getMyMessages/:tag",auth, tagsConroller.getTagMessages)
 module.exports = router;
