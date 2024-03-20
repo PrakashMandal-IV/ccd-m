ff60008c95eba503124432d9d80dd57bb65aa547
Prakash Mandal <prakashmandal.iv@gmail.com>
2024-03-20 18:35:18 +0530

    Add : route for get my tags

--- controllers/userController.js
+++ controllers/userController.js
@@ -102,6 +102,18 @@ exports.updateUserData = async (req, res) => {
     }
 }
 
+exports.getMyTags = async (req, res) => {
+    try {
+       
+        const userId = req.userId;
+        const user = await UserModel.findById(userId)
+
+        return res.success("Success", user.tags?.toString());
+    } catch (error) {
+        return res.error("Error occurred while creating user", error.message);
+    }
+}
+
 exports.changePassword = async (req, res) => {
     try {
         const { password } = req.body
