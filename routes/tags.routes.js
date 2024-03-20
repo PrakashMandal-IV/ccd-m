e39da6ec9a09c8b5a0af3b2b1b409fa906a3b5c3
Prakash Mandal <prakashmandal.iv@gmail.com>
2024-03-20 19:05:54 +0530

    Add : tag route fix

--- routes/tags.routes.js
+++ routes/tags.routes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router()
 const auth = require("../middleware/auth");
 
-const tagsConroller = require('../controllers/channelController')
+const tagsConroller = require('../controllers/tagController')
 
 router.get("/getMyTags",auth, tagsConroller.getMyTags)
 
